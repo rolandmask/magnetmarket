aea367bb8757aec9377b0513a20cad43c0a93c75
R0land <maskroland@gmail.com>
2019-08-23 21:26:20 +0300

    Navbar and styles cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import './home.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import logo from '../images/logo.svg'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://magnet.market/">
+        Magnet.Market
+      </Link>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}));
+
+
+
+ 
 
 const Home = () => {
-
-    return (
-      <div className="pager">
-        <p>Home page</p>
-      </div>
+  const classes = useStyles();
+    return  (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+        <img src={logo} alt="HTML5" width={100} height={100}></img>
+          
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     );
 
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,12 +4,12 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
 
     return (
-      <div id="mainNav" align="center">
-        <NavLink to="/">Home</NavLink> / 
-        <NavLink to="/About">About</NavLink> / 
-        <NavLink to="/Contact">Contact</NavLink> /
-        <NavLink to="/Login">Login</NavLink> /
-        <NavLink to="/Dashboard">Dashboard</NavLink>
+      <div id="mainNav">
+        <NavLink to="/">HOME</NavLink> / 
+        <NavLink to="/About">ABOUT</NavLink> / 
+        <NavLink to="/Contact">CONTACT</NavLink> /
+        <NavLink to="/Login">LOGIN</NavLink> /
+        <NavLink to="/Dashboard">DASHBOARD</NavLink>
       </div>
     );  
 
