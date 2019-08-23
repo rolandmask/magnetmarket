bd4e04c44ce4280796db9cd587606f5ba1781ac0
R0land <maskroland@gmail.com>
2019-08-23 05:16:34 +0300

    Cleanup Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
     return (
       <div className="pager">
- <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
+ <p>Home page</p>
       </div>
     );
 
