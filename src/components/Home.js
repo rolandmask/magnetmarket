a12b40ec949c72bf7662c93514b4287b1a8c3fbf
R0land <maskroland@gmail.com>
2019-08-23 04:57:07 +0300

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+
+    return (
+      <div className="pager">
+ <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
+      </div>
+    );
+
+};
+
+export default Home;
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+
+    return (
+      <div align="center">
+        <NavLink to="/">Home</NavLink> / 
+        <NavLink to="/About">About</NavLink> / 
+        <NavLink to="/Contact">Contact</NavLink>
+      </div>
+    );  
+
+};
+
+export default Navigation;
